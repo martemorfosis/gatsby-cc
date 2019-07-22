4160f3565d37503aab7f9316195d33d1a82a67af
Marte Baquerizo <martemorfosis@gmail.com>
2019-07-22 18:10:06 -0500

    some small changes SCSS working now

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
-import React from 'react'
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import './header.module.scss';
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  // style={{
+  //   background: `rebeccapurple`,
+  //   marginBottom: `1.45rem`,
+  // }}
   >
     <div
       style={{
@@ -16,7 +17,7 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 className="main">
         <Link
           to="/"
           style={{
@@ -29,14 +30,14 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
